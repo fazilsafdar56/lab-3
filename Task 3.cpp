c98b5255d279d4ab082c79dcc90def598a1c2ccc
fazilsafdar56 <fazilsafdar56@gmail.com>
2026-03-03 10:56:04 +0500

    Add files via upload

---
+++ Task 3.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+int main()
+
+    { int age =0;
+    cout<< "enter your age"<<endl;
+    cin>>age;
+    if (age>=18)
+cout<<"you are  eligible for vote"<<endl;
+if (age<18)
+cout<<"you are not eligible for vote"<<endl;
+
+
+
+
+    return 0;
+    }
---
+++ task 33.cpp
@@ -0,0 +1,19 @@
+
+
+#include <iostream>
+using namespace std;
+int main()
+
+    { int age =0;
+    cout<< "enter your age"<<endl;
+    cin>>=age;
+    if (age>18)
+cout<<"you are  eligible"<<endl;
+if (age<18)
+cout<<"you are not eligible"<<endl;
+
+
+
+
+    return 0;
+    }
