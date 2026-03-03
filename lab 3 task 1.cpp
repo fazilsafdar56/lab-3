bf9f20efe55dbcb51d4d6c525ef8f9869daea99f
fazilsafdar56 <fazilsafdar56@gmail.com>
2026-03-03 10:54:12 +0500

    Add files via upload

---
+++ lab 3 task 1.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	  int num1;
+	  int num2;
+	  cout<<"Enter first number"<<endl;
+	  cin>>num1;
+	  cout<<"Enter second number"<<endl;
+	  cin>>num2;
+	  if (num1<num2)
+	  
+	  cout<<num1<<"is less than "<<num2<<endl;
+	  else if (num1>num2)
+	  cout<<num1<<"is greater than"<<num2<<endl;
+	  else
+	  cout<<num1<<"  equal to"<<num2<<endl;
+	  
+	  
+
+
+
+    return 0;
+    }
